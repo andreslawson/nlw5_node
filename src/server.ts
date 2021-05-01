d03a95e14a0b9a34be7c52c0a622e350733b8c64
Andres Lawson <andresuk@live.com>
2021-05-01 20:04:33 +0100

    day 2

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,12 @@
 import express, { request, response } from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-/**
- * GET = Search
- * POST = Create
- * PUT = Change
- * DELETE = Delete
- * PATCH = Change specific information
- */
+app.use(express.json());
 
-app.get("/",(request, response) =>{
-  return response.json({
-    message: "Ola NLW 05!",
-  });
-
-});
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuario salvo com sucesso!",
-  });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
