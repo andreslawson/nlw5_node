094d5c13fdf2c428075008562fc729f35d73007d
Andres Lawson <andresuk@live.com>
2021-05-09 00:54:44 +0100

    day 4

--- src/services/UsersServices.ts
+++ src/services/UsersServices.ts
@@ -33,6 +33,12 @@ class UsersService {
 
   }
 
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({email});
+
+    return user;
+  }
+
 }
 
 export { UsersService };
